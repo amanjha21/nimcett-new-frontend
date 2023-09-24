42efa69c2096efba4c1c5f158512bb6a9fb252cd
amanjha21 <aman.jha21.aj@gmail.com>
2023-09-24 23:22:40 +0545

    init

---
+++ src/components/testComp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestComp = () => {
+  return <div>test component</div>;
+};
+
+export default TestComp;
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TestComp from "../components/testComp";
+
+const Home = () => {
+  return (
+    <div>
+      <div className='text-3xl font-bold underline'>Home</div>
+      <TestComp />
+    </div>
+  );
+};
+
+export default Home;
